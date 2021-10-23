91fc324bdc6cc1f399986375116192deab5caf62
KGITBank <KGITBank@DESKTOP-8R6CJUT>
2021-10-23 10:36:46 +0900

    faq

--- src/main/java/com/kgitbank/ebs/service/faqMapper.java
+++ src/main/java/com/kgitbank/ebs/service/faqMapper.java
@@ -21,4 +21,54 @@ public class faqMapper {
 		List<FaqDTO> list = sqlSession.selectList(namespace+".faqList");
 		return list;
 	}
+
+	public Object FaqlistCategory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object FaqreadTitle(int cno, String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object FaqtotalBnoCount(int cno, String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object Faqfadtorite() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object FaqFadtoriteTotalBnoCount(int cno, String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void FaqnewQuestion(String category, int cno, String question, String answer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Faqdelete(int cno, int bno) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object FaqupdateView(int cno, int bno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void Faqupdate(int cno, String category, String question, String answer, int bno) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Faqreadcount(int cno, int bno) {
+		// TODO Auto-generated method stub
+		
+	}
 }
